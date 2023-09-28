5b729357194f892ea9168bf617784172079c42b2
jbirch2002 <jbirch2002@gmail.com>
2023-09-27 20:06:39 -0400

    Added python files

---
+++ greetings.py
@@ -0,0 +1,3 @@
+user_name = input("Please enter your name: ")
+
+print("Hi " + user_name + "!")
---
+++ mablibs.py
@@ -0,0 +1,11 @@
+noun = input("Please enter your noun: ")
+adjective = input("Please enter your adjective: ")
+adverb = input("Please enter your adverb: ")
+verb = input("Please enter your verb: ")
+
+lyrics = "On a " + adjective + " island,\n" + \
+         "Underneath a " + noun + " lava moon.\n" + \
+         "Hangin' with the hula dancers,\n" + \
+         verb + " questions " + adverb + " cause' they got all the answers."
+
+print(lyrics)
